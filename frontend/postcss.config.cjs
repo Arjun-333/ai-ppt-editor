8045edd140020bdf8140c68eb5a7572767d91870
Arjun-333 <arjunr252005@gmail.com>
2025-11-19 20:38:52 +0530

    frontend updated

--- frontend/postcss.config.cjs
+++
@@ -1,5 +0,0 @@
-const tailwind = require("@tailwindcss/postcss");
-
-module.exports = {
-  plugins: [tailwind(), require("autoprefixer")],
-};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";   // <-- important
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
